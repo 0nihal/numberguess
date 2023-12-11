0bdd804a45a2c5c57d67110b271868887f7ecfb8
nihal <gulnihalaltun14@gmail.com>
2023-12-11 13:04:25 +0300

    html

---
+++ web/javasc.js
@@ -0,0 +1,64 @@
+'use strict';
+
+let secretNumber=Math.trunc(Math.random()* 20) + 1;//math.trunc integar dönüştürdü
+let score=20;
+let highscore=0;
+
+const displayMessage=function(message){
+    document.querySelector('.message').textContent=message;
+};
+document.querySelector('.check').addEventListener('click',function(){
+    const guess=Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    console.log(secretNumber, typeof secretNumber);
+
+    if(!guess){
+        displayMessage("Bir sayı giriniz.");
+    }
+    else if(guess===secretNumber){
+       
+        displayMessage('Doğru Tahmin...');
+        document.querySelector('.number').textContent=secretNumber;
+        document.querySelector('body').style.backgroundColor ='#60b347'
+        document.querySelector('.number').style.width='30 rem'
+        
+        
+        if(score > highscore){
+            highscore=score;
+            document.querySelector('.highscore').textContent=highscore;
+        }
+
+    }
+
+    else if(guess !== secretNumber){
+        if(score>1){
+            displayMessage(guess > secretNumber  ? ':Çok Yüksek':'Çok Düşük');
+            document.querySelector('.score').textContent=score;
+
+        }
+        else{
+            displayMessage('Oyun Bitti');
+            document.querySelector('.score').textContent=0;
+
+        }
+            
+        
+    }
+    
+});
+document.querySelector('.again').addEventListener('click',function(){
+    score=20;
+    secretNumber=Math.trunc(Math.random()*20)+1;
+
+     displayMessage('Tahmin Ediliyor')
+     document.querySelector('.score').textContent=score;
+     document.querySelector('.number').textContent='?';
+     document.querySelector('.guess').value='';
+
+     document.querySelector('body').style.backgroundColor='#000000';
+     document.querySelector('.number').style.width='15rem';
+  
+
+    
+});
+
